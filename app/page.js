2b3e3e35ebb22d291d13d71b755248e27e5dd8a8
Helena-Siqueira <156859073+Helena-Siqueira@users.noreply.github.com>
2025-02-13 21:29:08 -0300

    AulaPokedex QF

--- app/page.js
+++ app/page.js
@@ -1,9 +1,39 @@
-import Image from "next/image";
+'use client'
+
+import axios from "axios";
+import { useState } from "react";
+
 
 export default function Home() {
+
+  const [pokemons, alteraPokemons] = useState([]);
+  const [pesquisa, alteraPesquisa] = useState("");
+
+  async function busca(){
+     const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu")
+     const data = response.data
+     console.log(data);
+
+  }
+
   return (
       <div>
+
         <h1> Pokedex </h1>
+
+        <p> Digite o nome de um Pokémon:</p>
+        <input/>
+        <br/>
+        <button onClick={()=> busca() }>Pesquisar</button>
+
+        <hr/>
+
+        <h2> Nome </h2>
+        <p> <strong> ID: </strong> ?? </p>
+        <img src=""/>
+
+
       </div>
   );
 }
+
